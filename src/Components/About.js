d1a28ee3dc529713516c147a5fdb9a4efe7e4807
Annamalai-Deepak-12 <annamalaideepakr@gmail.com>
2023-05-05 12:36:03 +0200

    about commit 1

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ function About() {
       <div>
         <h1 className='m-5 px-5'>About</h1>
       </div>
-      <div className='m-5'>
+      <div className='col-lg-12 p-3'>
         <h4 className='col-lg-12 p-3'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.</h4>
         <h4 className='col-lg-12 p-3'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.</h4>
       </div>
